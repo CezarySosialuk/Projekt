92b04169a3a98ba9fd8bc210006097673df38e63
Cezary Sosialuk <137802658+CezarySosialuk@users.noreply.github.com>
2024-03-06 16:07:51 +0100

    Pierwszy szkic

--- Python files/Main.py
+++ Python files/Main.py
@@ -1,21 +1,21 @@
-#zapisywanie krótkich notatek (aplikacja do zapisywania tekstu brudnopis)
+#zapisywanie krótkich notatek (aplikacja do zapisywania tekstu brudnopis)#
 
 InMenu = True
-while(bool(InMenu)):
-
+while bool(InMenu):
+#menu#
     print("Zapisywanie notatek")
     print("1. Otwórz notatke")
     print("2. Napisz notatke")
     print("3. Usuń notatke")
     mode = input("Podaj numer funkcji:")
-
-    if (int(mode) == 1):
+#menu end#
+    if int(mode) == 1:
         print("tutaj pokazanie listy notatek")
         InMenu = False
-    elif (int(mode) == 2):
+    elif int(mode) == 2:
         print("tutaj zapisywanie nowej notatki")
         InMenu = False
-    elif (int(mode) == 3):
+    elif int(mode) == 3:
         print("tutaj usuwanie notatek")
         InMenu = False
     else:
@@ -23,5 +23,5 @@ while(bool(InMenu)):
         print("Wybrano niewłaściwą opcję")
         print("~")
 
-#with open("Notes/note1.txt", 'w') as f:
-#    f.write('doda czy usunie')
+#with open("Notes/note1.txt", 'w') as f:#
+#    f.write('text')#
