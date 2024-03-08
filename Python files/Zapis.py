550948fe6fa54a27e59b794d2351387db1df77b5
Cezary Sosialuk <137802658+CezarySosialuk@users.noreply.github.com>
2024-03-08 18:08:22 +0100

    naprawione zapisywanie

--- Python files/Zapis.py
+++ Python files/Zapis.py
@@ -8,13 +8,13 @@ def ZapisanieNotatki():
 
         f = codecs.open("Notes/" + name + ".txt", 'w', "utf=8")
     except:
-        print("nazwa i notatka nie mogą zawierać polskich znaków")
+        print("nazwa pliku ma zakazane znaki")
     else:
         note = input("twoja notatka:\n")
         try:
             f.write(note)
         except:
-            print("nazwa i notatka nie mogą zawierać polskich znaków")
+            print("w pliku są zakazane znaki")
         else:
             print("Notatka zapisana")
 
