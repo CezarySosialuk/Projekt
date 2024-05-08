762bf90925029a92549ceea1cb506adabbfc4770
Kitsune Zeppeli <137802658+CezarySosialuk@users.noreply.github.com>
2024-05-08 13:21:32 +0200

    wywalanie śmieci two electric boogaloo

--- Python files/Odczyt.py
+++
@@ -1,6 +0,0 @@
-def OdczytanieNotatki():
-    index = open("index.txt", 'r')
-    print(index.read())
-
-    odczyt = open("Notes/" + input("który plik chcesz otworzyć: ") + ".txt", 'r')
-    print(odczyt.read())
